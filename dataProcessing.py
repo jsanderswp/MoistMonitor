141a090ed9eea5c6edec765342b1bb6002ac588f
jsanderswp <115518944+jsanderswp@users.noreply.github.com>
2024-12-04 17:02:02 -0800

    Update dataProcessing.py

--- dataProcessing.py
+++ dataProcessing.py
@@ -2,3 +2,20 @@ import csv
 import time
 import serial
 
+ser = serial.Serial('INSERT PORT NAME', timeout=1)
+
+f = open("moistureData.csv", "a+")
+writer = csv.writer(f, delimiter=', ')
+
+while True:
+    s = ser.readline().decode()
+    if s != "":
+        rows = [int(x) for x in s.split(', ')]
+        print(rows)
+        writer.writerow(rows)
+        f.flush()
+
+#left to do: iterate thru CSV, check for moisture values below threshold, send twilio message if so
+
+
+
