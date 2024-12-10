642737287fdaa8d387aa4da27cb665a5a254bcc8
jsanderswp <115518944+jsanderswp@users.noreply.github.com>
2024-12-09 23:08:11 -0800

    Update dataProcessing.py
    organized CSV writing into one function

--- dataProcessing.py
+++ dataProcessing.py
@@ -2,19 +2,22 @@ import csv
 import time
 import serial
 
-ser = serial.Serial('INSERT PORT NAME', timeout=1)
+import csv
+import time
+import serial
 
-f = open("moistureData.csv", "a+")
-writer = csv.writer(f, delimiter=', ')
+def read_serial_data():
+    ser = serial.Serial('/dev/cu.usbmodem101', timeout=1)
+    with open("moistureData.csv", "a+") as f:
+        writer = csv.writer(f, delimiter=',')
 
-while True:
-    s = ser.readline().decode()
-    if s != "":
-        rows = [int(x) for x in s.split(', ')]
-        print(rows)
-        writer.writerow(rows)
-        f.flush()
-
+        while True:
+            s = ser.readline().decode()
+            if s != "":
+                rows = [int(x) for x in s.split(',')]
+                print(rows)
+                writer.writerow(rows)
+                f.flush()
 #left to do: iterate thru CSV, check for moisture values below threshold, send twilio message if so
 
 
