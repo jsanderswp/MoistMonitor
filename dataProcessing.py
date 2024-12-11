3f6c5990722aeacd84c03af4e1322815a15baf32
jsanderswp <115518944+jsanderswp@users.noreply.github.com>
2024-12-10 16:01:50 -0800

    Update dataProcessing.py

--- dataProcessing.py
+++ dataProcessing.py
@@ -6,10 +6,10 @@ import serial
 from twilio.rest import Client
 
 account_sid = 'AC09190c2e1005f08c38b13b61d2f3fab2'
-auth_token = '[AuthToken]'
+auth_token = 'c230b92bcb6a4b9f2b320addb9b4e260'
 client = Client(account_sid, auth_token)
 
-threshold = 300  # Example moisture threshold
+threshold = 400  # Example moisture threshold
 
 def check_moisture_and_notify():
     with open("moistureData.csv", "r") as f:
@@ -19,8 +19,8 @@ def check_moisture_and_notify():
                 value = int(row[0])  # Adjust index if more columns exist
                 if value < threshold:
                     message = client.messages.create(messaging_service_sid='MG5f417675f7ec606293dce1bd89ac59eb',
-                    body='Water your plant!',
-                    to='+15052695439'
+                    body='This is being sent thru Python!',
+                    to='+18777804236'
                     )
                     print(f"Message sent: {message.sid}")
                     break
@@ -32,7 +32,7 @@ def check_moisture_and_notify():
 # function: read_serial data
 
 def read_serial_data():
-    ser = serial.Serial('/dev/cu.usbmodem101', timeout=1)
+    ser = serial.Serial('/dev/cu.usbmodem1101', timeout=1)
     with open("moistureData.csv", "a+") as f:
         writer = csv.writer(f, delimiter=',')
 
@@ -40,12 +40,11 @@ def read_serial_data():
             s = ser.readline().decode()
             if s != "":
                 rows = [int(x) for x in s.split(',')]
-                print(rows)
                 writer.writerow(rows)
                 f.flush()
 
 def main():
     read_serial_data()
     check_moisture_and_notify()
-
+    print("This is running")
 main()
